556965a4324aa8dc34d3edba98d5ff669afc3c1d
fahamdev <faham.dev@gmail.com>
2021-10-11 16:47:18 +0600

    add land-details

--- src/crawler/crawler.module.ts
+++ src/crawler/crawler.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { NestCrawlerModule } from 'nest-crawler';
 import { CrawlerService } from './crawler.service';
-import { LandDetail, LandDetailSchema } from './entities/land-detail.entity';
 
 @Module({
-  imports: [
-    NestCrawlerModule,
-    MongooseModule.forFeature([
-      {
-        name: LandDetail.name,
-        schema: LandDetailSchema,
-      },
-    ]),
-  ],
+  imports: [NestCrawlerModule],
   providers: [CrawlerService],
 })
 export class CrawlerModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'verbose', 'debug'],
+  });
 
   app.use(helmet());
   app.enableCors();
@@ -21,6 +24,15 @@ async function bootstrap() {
   );
 
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest-Mongo-Crawler')
+    .setDescription('A simple Crawler')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/', app, document);
 
   await app.listen(3000);
 }
